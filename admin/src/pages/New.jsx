0770343f09ac441bff53340c097a0e9c1b2cbc8d
Debdeep1 <deb.baner.1@gmail.com>
2022-06-02 10:09:11 +0530

    kaj krchena

--- admin/src/pages/New.jsx
+++ admin/src/pages/New.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import "./styles/new.scss"
+import SideBar from "../components/SideBar"
+import Navbar from "../components/Navbar"
 function New() {
   return (
-    <div>New</div>
+    <div classNmae='new'>
+    <SideBar/>
+      <div className='newContainer'>
+        <Navbar/>
+        <div className='top'>
+          <h1>Add new user</h1>
+        </div>
+        <div className='bottom'>
+          <div className='left'>left</div>
+          <div className='right'>right</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
